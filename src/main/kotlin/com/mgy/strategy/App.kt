d39290d4ca8f4b7f14bd50a87e505462a6afc38b
drunkman <2052943631@qq.com>
2018-07-02 21:02:11 +0800

    add strategy design pattern class diagram

--- src/main/kotlin/com/mgy/strategy/App.kt
+++ src/main/kotlin/com/mgy/strategy/App.kt
@@ -5,9 +5,7 @@ package com.mgy.strategy
  * The Strategy pattern (also known as the policy pattern) is a software design pattern that enables
  * an algorithm's behavior to be selected at runtime.
  * <p>
- * Before Java 8 the Strategies needed to be separate classes forcing the developer
- * to write lots of boilerplate code. With modern Java it is easy to pass behavior
- * with method references and lambdas making the code shorter and more readable.
+ * In kotlin, you needn't defined any interfaces and subclass by using kotlin lambdas.
  * <p>
  * In this example ({@link DragonSlayingStrategy}) encapsulates an algorithm. The containing object
  * ({@link DragonSlayer}) can alter its behavior by changing its strategy.
